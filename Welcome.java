b40a83725b9848ccee5c2e34c457f66f9ed18e9a
Itashi20 <144596083+Itashi20@users.noreply.github.com>
2023-11-01 01:37:56 -0700

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,5 @@
 
 public class Welcome{
     public static void main(String args[]){  
-     System.out.println("Hello Java update
-                        ");  
+     System.out.println("Hello Java update");  
 }}
